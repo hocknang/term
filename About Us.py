70b400f4048c81e1c94b27c686f24860095fc6c0
peiyuan <fang_peiyuan@yahoo.com>
2024-10-08 22:25:32 +0800

    test

---
+++ About Us.py
@@ -0,0 +1,9 @@
+#import streamlit library
+
+import streamlit as st
+
+st.set_page_config(
+    page_title="EmPOWER"
+)
+
+st.title("EmPOWER")
---
+++ pages/2_ChatBot.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="EmPOWER"
+)
+
+st.title("Ask TADM")
