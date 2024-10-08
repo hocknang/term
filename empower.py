bf3ba9af9276a9bc2c21713e826de16f19a1cb55
peiyuan <fang_peiyuan@yahoo.com>
2024-10-08 23:33:14 +0800

    new

---
+++ empower.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+
+def home():
+    st.title("EmPOWER")
+    st.write('Welcome to the EmPOWER Page.')
--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import streamlit as st
+import empower as empower
+
 
 # Define the pages
 def page_home():
-    st.title("EmPOWER")
-    st.write("Welcome to the EmPOWER Page.")
-
+    empower.home()
+    
 def page_about():
     st.title("About")
     st.write("This is the About Page.")
 
+
 def page_awards():
     st.title("Awards")
     st.write("Here is the Contact Page.")
+
 
 # Create a dictionary of pages
 pages = {
