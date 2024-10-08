592a556359d0804ab2d403f21ef5a7ad390cb824
peiyuan <fang_peiyuan@yahoo.com>
2024-10-09 00:14:20 +0800

    new

--- asktadm.py
+++ asktadm.py
@@ -1,4 +1,9 @@
 import streamlit as st
 
 def home():
-    st.title("Ask TADM")
+    st.write("a logo and text next to eachother")
+    col1, mid, col2 = st.beta_columns([1, 1, 20])
+    with col1:
+        st.image('https://github.com/hocknang/term/blob/main/picture/tadm%20icon.png', width=60)
+    with col2:
+        st.write('A Name')
