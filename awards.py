223a3aee272b482f56bd8486b69047389689d736
peiyuan <fang_peiyuan@yahoo.com>
2024-10-09 01:23:13 +0800

    new

--- awards.py
+++ awards.py
@@ -1,144 +1,140 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
+
 def home():
     st.title("Awards And Events")
-    components.html(
-        """
-    <!DOCTYPE html>
-    <html>
-    <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <style>
-    * {box-sizing: border-box;}
-    body {font-family: Verdana, sans-serif;}
-    .mySlides {display: none;}
-    img {vertical-align: middle;}
 
-    /* Slideshow container */
-    .slideshow-container {
-      max-width: 1000px;
-      position: relative;
-      margin: auto;
-    }
+    st.components.html(
+    """
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+* {box-sizing: border-box;}
+body {font-family: Verdana, sans-serif;}
+.mySlides {display: none;}
+img {vertical-align: middle;}
 
-    /* Caption text */
-    .text {
-      color: #f2f2f2;
-      font-size: 15px;
-      padding: 8px 12px;
-      position: absolute;
-      bottom: 8px;
-      width: 100%;
-      text-align: center;
-    }
+/* Slideshow container */
+.slideshow-container {
+  max-width: 1000px;
+  position: relative;
+  margin: auto;
+}
 
-    /* Number text (1/3 etc) */
-    .numbertext {
-      color: #f2f2f2;
-      font-size: 12px;
-      padding: 8px 12px;
-      position: absolute;
-      top: 0;
-    }
+/* Caption text */
+.text {
+  color: #f2f2f2;
+  font-size: 15px;
+  padding: 8px 12px;
+  position: absolute;
+  bottom: 8px;
+  width: 100%;
+  text-align: center;
+}
 
-    /* The dots/bullets/indicators */
-    .dot {
-      height: 15px;
-      width: 15px;
-      margin: 0 2px;
-      background-color: #bbb;
-      border-radius: 50%;
-      display: inline-block;
-      transition: background-color 0.6s ease;
-    }
+/* Number text (1/3 etc) */
+.numbertext {
+  color: #f2f2f2;
+  font-size: 12px;
+  padding: 8px 12px;
+  position: absolute;
+  top: 0;
+}
 
-    .active {
-      background-color: #717171;
-    }
+/* The dots/bullets/indicators */
+.dot {
+  height: 15px;
+  width: 15px;
+  margin: 0 2px;
+  background-color: #bbb;
+  border-radius: 50%;
+  display: inline-block;
+  transition: background-color 0.6s ease;
+}
 
-    /* Fading animation */
-    .fade {
-      animation-name: fade;
-      animation-duration: 1.5s;
-    }
+.active {
+  background-color: #717171;
+}
 
-    @keyframes fade {
-      from {opacity: .4} 
-      to {opacity: 1}
-    }
+/* Fading animation */
+.fade {
+  animation-name: fade;
+  animation-duration: 1.5s;
+}
 
-    /* On smaller screens, decrease text size */
-    @media only screen and (max-width: 300px) {
-      .text {font-size: 11px}
-    }
-    </style>
-    </head>
-    <body>
+@keyframes fade {
+  from {opacity: .4} 
+  to {opacity: 1}
+}
 
-    <h2>Automatic Slideshow</h2>
-    <p>Change image every 2 seconds:</p>
+/* On smaller screens, decrease text size */
+@media only screen and (max-width: 300px) {
+  .text {font-size: 11px}
+}
+</style>
+</head>
+<body>
 
-    <div class="slideshow-container">
+<h2>Automatic Slideshow</h2>
+<p>Change image every 2 seconds:</p>
 
-    <div class="mySlides fade">
-      <div class="numbertext">1 / 4</div>
-      <img src="https://unsplash.com/photos/GJ8ZQV7eGmU/download?force=true&w=1920" style="width:100%">
-      <div class="text">Caption Text</div>
-    </div>
+<div class="slideshow-container">
 
-    <div class="mySlides fade">
-      <div class="numbertext">2 / 4</div>
-      <img src="https://unsplash.com/photos/eHlVZcSrjfg/download?force=true&w=1920" style="width:100%">
-      <div class="text">Caption Two</div>
-    </div>
+<div class="mySlides fade">
+  <div class="numbertext">1 / 3</div>
+  <img src="https://unsplash.com/photos/GJ8ZQV7eGmU/download?force=true&w=1920" style="width:100%">
+  <div class="text">Caption Text</div>
+</div>
 
-    <div class="mySlides fade">
-      <div class="numbertext">3 / 4</div>
-      <img src="https://unsplash.com/photos/zVhYcSjd7-Q/download?force=true&w=1920" style="width:100%">
-      <div class="text">Caption Three</div>
-    </div>
-    
-    <div class="mySlides fade">
-      <div class="numbertext">4 / 4</div>
-      <img src="https://raw.githubusercontent.com/hocknang/term/refs/heads/main/picture/tadm%20icon.png" style="width:100%">
-      <div class="text">Caption Three</div>
-    </div>
+<div class="mySlides fade">
+  <div class="numbertext">2 / 3</div>
+  <img src="https://unsplash.com/photos/eHlVZcSrjfg/download?force=true&w=1920" style="width:100%">
+  <div class="text">Caption Two</div>
+</div>
 
-    </div>
-    <br>
+<div class="mySlides fade">
+  <div class="numbertext">3 / 3</div>
+  <img src="https://unsplash.com/photos/zVhYcSjd7-Q/download?force=true&w=1920" style="width:100%">
+  <div class="text">Caption Three</div>
+</div>
 
-    <div style="text-align:center">
-      <span class="dot"></span> 
-      <span class="dot"></span> 
-      <span class="dot"></span> 
-    </div>
+</div>
+<br>
 
-    <script>
-    let slideIndex = 0;
-    showSlides();
+<div style="text-align:center">
+  <span class="dot"></span> 
+  <span class="dot"></span> 
+  <span class="dot"></span> 
+</div>
 
-    function showSlides() {
-      let i;
-      let slides = document.getElementsByClassName("mySlides");
-      let dots = document.getElementsByClassName("dot");
-      for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";  
-      }
-      slideIndex++;
-      if (slideIndex > slides.length) {slideIndex = 1}    
-      for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-      }
-      slides[slideIndex-1].style.display = "block";  
-      dots[slideIndex-1].className += " active";
-      setTimeout(showSlides, 2000); // Change image every 2 seconds
-    }
-    </script>
+<script>
+let slideIndex = 0;
+showSlides();
 
-    </body>
-    </html> 
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+</script>
 
-        """,
-        height=600,
-    )
+</body>
+</html> 
+
+    """,
+    height=600,
+)
