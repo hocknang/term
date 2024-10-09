2e0c6beb5bef1855a7760651ba33b751c1a69afc
peiyuan <fang_peiyuan@yahoo.com>
2024-10-09 11:55:39 +0800

    new

--- video.py
+++ video.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 
 def home():
-    video_file = open("https://www.youtube.com/watch?v=u_C-UpCaRFM", "rb")
+    video_file = open("https://pixabay.com/en/videos/star-long-exposure-starry-sky-sky-6962/", "rb")
     video_bytes = video_file.read()
 
     st.video(video_bytes)
