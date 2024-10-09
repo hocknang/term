0757a95f5bb84a1a4519fd821c8928a846ef8d7e
peiyuan <fang_peiyuan@yahoo.com>
2024-10-09 12:11:52 +0800

    new

--- video.py
+++ video.py
@@ -5,7 +5,7 @@ def home():
     st.title("Video Player App")
 
     # Add a video from an online source (e.g., YouTube or other video hosting services)
-    video_url = "https://pixabay.com/en/videos/star-long-exposure-starry-sky-sky-6962/"
+    video_url = "https://cdn.pixabay.com/video/2016/12/31/6962-197634410_large.mp4"
 
     # Display the video in the Streamlit app
     st.video(video_url)
