8238591fbf0572f6c208f1a250e19a00e6691578
hocknang <fang_peiyuan@yahoo.com>
2024-10-15 10:55:16 +0800

    Update video.py

--- video.py
+++ video.py
@@ -5,7 +5,7 @@ def home():
     st.title("Video")
 
     # Add a video from an online source (e.g., YouTube or other video hosting services)
-    video_url = "https://cdn.pixabay.com/video/2016/12/31/6962-197634410_large.mp4"
+    video_url = "https://www.dropbox.com/scl/fi/b17vgeq6hmj92sy5xr8op/demoVideo.mp4?rlkey=551t3mndi0rxiowjw6ei3s2d6&st=n16gwygh&dl=0"
 
     # Display the video in the Streamlit app
     st.video(video_url)
